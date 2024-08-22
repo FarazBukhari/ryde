e74446446e3eac37786ed90d8d83cdba7c06b039
Faraz Bukhari <faraz.bukhari@conovoinc.com>
2024-08-22 13:08:19 +0500

    google places api search bar implemented find ride route ride layout setup

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -15,6 +15,7 @@ import Map from "@/components/Map";
 import { useLocationStore } from "@/store";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { router } from "expo-router";
 
 const recentRides = [
   {
@@ -132,7 +133,15 @@ export default function Page() {
   const [hasPermissions, setHasPermissions] = useState(false);
 
   const handleSignOut = () => {};
-  const handleDestinationPress = () => {};
+  const handleDestinationPress = (location: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    setDestinationLocation(location);
+
+    router.push("/(root)/find-ride");
+  };
 
   useEffect(() => {
     const requestLocation = async () => {
---
+++ app/(root)/find-ride.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import { useLocationStore } from "@/store";
+import RideLayout from "@/components/RideLayout";
+
+const FindRide = () => {
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
+  return (
+    <RideLayout>
+      <Text className="text-2xl">Find Ride</Text>
+    </RideLayout>
+  );
+};
+
+export default FindRide;
